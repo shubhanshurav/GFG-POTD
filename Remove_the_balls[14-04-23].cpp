f4a788304dd3eb0bdbd40fa5bef8ceed7c11253f
shubhanshurav <shubhanshurav63@gmail.com>
2023-04-14 09:55:12 +0530

    [14-04-23]

---
+++ Remove_the_balls[14-04-23].cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int finLength(int N, vector<int> color, vector<int> radius) {
+        stack<int> st1,st2;
+        st1.push(color[0]);
+        st2.push(radius[0]);
+
+        for(int i=1;i<N;i++){
+            if(!st1.empty() && st1.top()==color[i] && st2.top()==radius[i]){
+                st1.pop();
+                st2.pop();
+            }
+            else{
+                st1.push(color[i]);
+                st2.push(radius[i]);
+            }
+        }
+        return st1.size();
+    }
+};
+
+
+int main() {
+
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+
+        int n;
+        cin >> n;
+        vector<int> color(n), radius(n);
+        for (int i = 0; i < n; i++) cin >> color[i];
+        for (int i = 0; i < n; i++) cin >> radius[i];
+        int ans = sol.finLength(n, color, radius);
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
